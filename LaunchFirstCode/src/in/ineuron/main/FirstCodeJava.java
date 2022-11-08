d0458fe080b19a9672f082bb92218b181c2c847a
Yegishetty Vamshi Krishna <yegishettyvamshi@gmail.com>
2022-11-08 22:13:25 +0530

    First Commit

---
+++ LaunchFirstCode/src/in/ineuron/main/FirstCodeJava.java
@@ -0,0 +1,11 @@
+package in.ineuron.main;
+
+public class FirstCodeJava {
+	public static void main(String args[])
+	{
+	StringBuilder ob = new StringBuilder("Vamshi");
+	ob.append("Vamshi");
+	System.out.println(ob);
+	}
+
+}
